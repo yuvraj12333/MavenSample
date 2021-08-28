4a504ca3490f12d79e2517b22bc458ddaea1a9ea
yuvrajsingh123 <yuvi0622@gmail.com>
2021-08-28 13:33:43 +0530

    descriptive comment for code changes

---
+++ src/test/java/com/codebind/AppTest.java
@@ -0,0 +1,15 @@
+package com.codebind;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppTest {
+
+	@Test
+	public void test() {
+		String take=App.Sample();
+		System.out.println(take);
+	}
+
+}
